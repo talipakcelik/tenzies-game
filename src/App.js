d2cbcc739daf4906b55f369b748f5f7d80af0632
Talip <talipakcelik@gmail.com>
2022-06-09 16:33:15 +0300

    update useEffect function 2

--- src/App.js
+++ src/App.js
@@ -6,17 +6,24 @@ function App() {
 
   const [tenzies, setTenzies] = React.useState(false);
 
-  React.useEffect(() => {
-    return console.log("Dice state changed");
-  }, [dice]);
-
   /**
-   * Challenge:
-   * 1. Add new state called `tenzies`, default to false. It
-   *    represents whether the user has won the game yet or not.
-   * 2. Add an effect that runs every time the `dice` state array
-   *    changes. For now, just console.log("Dice state changed").
+   * Challenge: Check the dice array for these winning conditions:
+   * 1. All dice are held, and
+   * 2. all dice have the same value
+   *
+   * If both conditions are true, set `tenzies` to true and log
+   * "You won!" to the console
    */
+
+  React.useEffect(() => {
+    if (
+      dice.every((die) => die.isHeld) &&
+      dice.every((die) => die.value === dice[0].value)
+    ) {
+      setTenzies(true);
+      console.log("you won");
+    }
+  }, [dice]);
 
   function allNewDice() {
     const newDice = [];
