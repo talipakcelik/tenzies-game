4e797c1f681127ba2258e76e8ec463e3ab0a922d
Talip <talipakcelik@gmail.com>
2022-06-09 11:26:56 +0300

    add onClick event attribute

--- src/Die.js
+++ src/Die.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Die(props) {
   return (
-    <div className={props.isHeld ? "die held" : "die"}>
+    <div className={props.isHeld ? "die held" : "die"} onClick={props.hold}>
       <span>{props.value}</span>
     </div>
   );
