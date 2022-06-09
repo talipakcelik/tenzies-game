3a3fae0154c1bed99e3080975cbe9c91f9c3d391
Talip <talipakcelik@gmail.com>
2022-06-09 07:55:47 +0300

    add allNewDice function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React from "react";
 import Die from "./Die";
 
 function App() {
+  function allNewDice() {
+    const random = Array(10)
+      .fill()
+      .map(() => Math.ceil(Math.random() * 6));
+
+    return random;
+  }
+
   return (
     <main>
       <div className="die-container">
