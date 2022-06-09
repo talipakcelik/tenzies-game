889cb623c347cf60b227e4bc5ae699a0f3935829
Talip <talipakcelik@gmail.com>
2022-06-09 08:10:22 +0300

    add state for dice, map over it and render elements

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
 import Die from "./Die";
 
+/**
+ * Challenge:
+ *
+ * Create state to hold our array of numbers. (Initialize
+ * the state by calling our `allNewDice` function so it
+ * loads all new dice as soon as the app loads)
+ *
+ * Map over the state numbers array to generate our array
+ * of Die elements and render those in place of our
+ * manually-written 10 Die elements.
+ */
+
 function App() {
+  const [dice, setDice] = React.useState(allNewDice());
+
+  const diceElements = dice.map((die, index) => (
+    <Die key={index} value={die} />
+  ));
+
   function allNewDice() {
     const random = Array(10)
       .fill()
@@ -12,18 +30,7 @@ function App() {
 
   return (
     <main>
-      <div className="die-container">
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-      </div>
+      <div className="die-container">{diceElements}</div>
     </main>
   );
 }
